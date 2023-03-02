7502895cfc9a1d80367b48aab80c85b6ec934996
Filip <filip4125@gmail.com>
2023-03-02 17:22:10 +0100

    Implementirana funkcionalnost skakanja

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     // Biće kreirana funkcionalnost za mobile i pc
 
     Rigidbody2D myRigidBody;
+    BoxCollider2D myCollider;
 
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 2f;
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
         // GetAxisRaw() utiče na to da igrač ne klizi, dakle kad se pusti dugme setuje brzinu na 0 odmah
         myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidBody.velocity.y);
+
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Debug.Log("Dodiruje");
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+
+        // myRigidBody.velocity = new Vector2(value.Get<Vector2>() * moveSpeed, myRigidBody.velocity.y);
     }
 }
