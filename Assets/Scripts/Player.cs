e4a45272a945f8865a27590d258e4498e4703129
Filip <filip4125@gmail.com>
2023-03-02 16:09:12 +0100

    Uveženi aseti, dodat igrač i komponente na njegovom objektu i urađena funkcionalnost horizontalnog kretanja

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Biće kreirana funkcionalnost za mobile i pc
+
+    Rigidbody2D myRigidBody;
+
+    [SerializeField] float moveSpeed = 5f;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // GetAxisRaw() utiče na to da igrač ne klizi, dakle kad se pusti dugme setuje brzinu na 0 odmah
+        myRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidBody.velocity.y);
+    }
+}
