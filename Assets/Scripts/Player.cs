385079d5ad08e19705c0cc8f099ce50b4f6f5c2f
Filip <filip4125@gmail.com>
2023-03-09 16:24:29 +0100

    Kreirana i sređena animacija za efekat udarca mačem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour
     BoxCollider2D myCollider;
     SpriteRenderer mySpriteRenderer;
     Animator myAnimator;
+    // Potrebna referenca animacije za efekat sečenja 
+    Animator mySwordAnimation;
+    SpriteRenderer mySwordSpriteRender;
 
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpForce = 2f;
@@ -22,7 +25,11 @@ public class Player : MonoBehaviour
         myRigidBody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<BoxCollider2D>();
         mySpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
         myAnimator = GetComponentInChildren<Animator>();
+        // Na ovaj naćin dolazimo direkt do podobjekta i referenciramo na njegovu komponentu animator
+        mySwordAnimation = transform.GetChild(1).GetComponent<Animator>();
+        mySwordSpriteRender = transform.GetChild(1).GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -56,6 +63,16 @@ public class Player : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    void OnFire(InputValue value)
+    {
+
+        if (value.isPressed && myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myAnimator.SetTrigger("Attack");
+            mySwordAnimation.SetTrigger("SwordAnimation");
+        }
+    }
+
     void Run()
     {
         myRigidBody.velocity = new Vector2(moveInput.x * moveSpeed, myRigidBody.velocity.y);
@@ -71,10 +88,18 @@ public class Player : MonoBehaviour
             if (Mathf.Sign(myRigidBody.velocity.x) == 1)
             {
                 mySpriteRenderer.flipX = false;
+
+                mySwordSpriteRender.transform.localPosition = new Vector3(1.01f, mySwordSpriteRender.transform.localPosition.y, mySwordSpriteRender.transform.localPosition.z);
+                mySwordSpriteRender.flipX = false;
+                mySwordSpriteRender.flipY = false;
             }
             else if (Mathf.Sign(myRigidBody.velocity.x) == -1)
             {
                 mySpriteRenderer.flipX = true;
+
+                mySwordSpriteRender.transform.localPosition = new Vector3(-1.01f, mySwordSpriteRender.transform.localPosition.y, mySwordSpriteRender.transform.localPosition.z);
+                mySwordSpriteRender.flipX = true;
+                mySwordSpriteRender.flipY = true;
             }
         }
     }
