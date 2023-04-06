b28535bd1dc1a8b48440d8fde22b38df509d2e9b
Filip <filip4125@gmail.com>
2023-04-06 15:33:18 +0200

    Dodat hud na ekranu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    // Instance predstavlja property, kog koristimo za pristup privatnom polju instance
+    public static GameManager Instance
+    {
+        get
+        {
+            // Vrši se provera da li je kreirana instanca koja referencira na klasu GameManager
+            if (instance == null)
+                Debug.Log("GameManager is null");
+            return instance;
+        }
+    }
+
+    public bool HasKeyToCastle { get; set; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
--- Assets/Scripts/ShopMenu.cs
+++ Assets/Scripts/ShopMenu.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +6,16 @@ public class ShopMenu : MonoBehaviour
 {
     [SerializeField]
     GameObject shopPanel;
+    public int currentSelectedItem;
+    public int currentItemCost;
+
+    Player player;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Player player = other.GetComponent<Player>();
+            player = other.GetComponent<Player>();
             if (player != null)
             {
                 UIManager.Instance.OpenShop(player.diamond);
@@ -35,14 +39,41 @@ public class ShopMenu : MonoBehaviour
         {
             case 0:
                 UIManager.Instance.UpdateShopSelection(85);
+                currentSelectedItem = 0;
+                currentItemCost = 200;
                 break;
             case 1:
                 UIManager.Instance.UpdateShopSelection(-25);
+                currentSelectedItem = 1;
+                currentItemCost = 400;
                 break;
             case 2:
                 UIManager.Instance.UpdateShopSelection(-135);
+                currentSelectedItem = 2;
+                currentItemCost = 100;
                 break;
 
+        }
+    }
+
+    public void BuyItem()
+    {
+        if (player.diamond >= currentItemCost)
+        {
+            // Potrebno je da proverimo da li je odabran ključ tj. stavka 2. Fora je što je ključ uslov za pobedu
+            if (currentSelectedItem == 2)
+            {
+                GameManager.Instance.HasKeyToCastle = true;
+            }
+            player.diamond -= currentItemCost;
+            Debug.Log("Kupili ste ajtem: " + currentSelectedItem);
+            Debug.Log("Trenutan broj dijamanata: " + player.diamond);
+            shopPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Nemate dovoljno dijaanata za ovaj ajtem");
+            shopPanel.SetActive(false);
         }
     }
 }
